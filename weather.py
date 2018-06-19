2264e352b15a7ec6e71daf08960af41d09077198
Sean McGlincy <swmcglincy@gmail.com>
2018-06-19 00:04:37 -0400

    adding wind chill equation

--- weather.py
+++ weather.py
@@ -6,10 +6,9 @@ import pandas as pd
 
 def reformat_data(weather_data):
     for key, value in weather_data.items():
-
         value["DATE"], value["TIME"] = value["DATE"].str.split("\s+", 1).str
         value["TIME"] = value["TIME"].str.replace(':', '').astype(int)
-
+    #    todo check time for NAN pd.to_numeric(data["HOURLYDRYBULBTEMPF"], errors='coerce').fillna(1000).astype(np.int64) < 40.0)]
     return weather_data
 
 
@@ -33,6 +32,29 @@ def avg_temp(date):
     return results
 
 
+def wind_chill_equation(temp, vilocity):
+    #  Cite US Windchill 2001:  https://en.wikipedia.org/wiki/Wind_chill
+    vilocity = vilocity ** 0.16
+    return 35.74 + 0.6215 * temp - 35.75 *  vilocity + 0.4275 * temp * vilocity
+
+
+def wind_chill(date):
+    results = {}
+    # todo perform check on dates
+    results = {}
+    for key, value in weather_data.items():
+        results[key] = {}
+
+
+
+        data = value.loc[(date == value["DATE"]) ]
+        data = data.loc[(pd.to_numeric(data["HOURLYDRYBULBTEMPF"], errors='coerce').fillna(1000).astype(np.int64) < 40)]
+        print (data["HOURLYDRYBULBTEMPF"])
+        wind_chill_equation()
+
+    # return results
+
+
 if __name__ == "__main__":
     # todo perform checks on input data
     weather_data = {}
@@ -44,10 +66,15 @@ if __name__ == "__main__":
 
 
     # Call Function
-    temp_data = avg_temp("10/3/17")
-    print (temp_data["ATL"]["TEMP_AVG"]["FAHRENHEIT"])
-    print (temp_data["ATL"]["TEMP_AVG"]["CELSIUS"])
-    print (temp_data["ATL"]["TEMP_STD"]["FAHRENHEIT"])
-    print (temp_data["ATL"]["TEMP_STD"]["CELSIUS"])
+    # temp_data = avg_temp("10/3/17")
+    # print (temp_data["ATL"]["TEMP_AVG"]["FAHRENHEIT"])
+    # print (temp_data["ATL"]["TEMP_AVG"]["CELSIUS"])
+    # print (temp_data["ATL"]["TEMP_STD"]["FAHRENHEIT"])
+    # print (temp_data["ATL"]["TEMP_STD"]["CELSIUS"])
+
+    temp_data = wind_chill("1/1/08")
+
+
+
 
 
