4e35a6f7595291145c5b28373ec12e22604e61d1
Sean McGlincy <swmcglincy@gmail.com>
2018-06-19 22:59:56 -0400

    cos working

--- weather.py
+++ weather.py
@@ -1,7 +1,8 @@
-import re
+# from scipy.spatial.distance import cosine
 import numpy as np
 import pandas as pd
-
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import spatial
 
 
 def reformat_data(weather_data):
@@ -55,36 +56,141 @@ def wind_chill(weather_data, date):
         results[key] = list(WC)
     return results
 
+
+
+def cos(x,y):
+    # if x is None or y is None:
+    #     return 0.0
+    print ("Cos Function")
+    # x = np.array(x)
+    # y = np.array(y)
+    print(x)
+    print(y)
+    # Uses distance, subtract 1 for similarity
+    cos = 1 - spatial.distance.cosine(x, y)
+    # cos = cosine(np.array(x), np.array(y))
+
+    # if w2v:
+    #     cos = cosine(np.array(x), np.array(y))
+    # else:
+    #     cos = cosine(x.toarray()[0], y.toarray()[0])
+    # cos = cosine_similarity(x, y)
+    print (cos)
+    if np.isfinite(cos):
+        return cos
+    return 0.0
+
+
+
 def process_data(df, date):
     #     todo assert data frame
-    # new_row = np.array([date])
-    # new_row.append(date)
     row = []
     row.append(date)
     df = df.loc[:, ["HOURLYVISIBILITY" , "HOURLYDRYBULBTEMPF", "HOURLYWETBULBTEMPF", "HOURLYRelativeHumidity", "HOURLYWindSpeed", "HOURLYWindDirection", "HOURLYWindGustSpeed"]]
-    # new_row.extend( df.fillna(0).values().flatten('F'))
-    t = df.fillna(0).as_matrix().flatten('F')
-    row.extend(t)
-    print(row)
 
-    # new_row.extend( df.fillna(0).as_matrix().flatten('F'))
-    # new_row.concatenate df.fillna(0).as_matrix().flatten('F'))
+    # df["HOURLYVISIBILITY"] = pd.to_numeric(df["HOURLYVISIBILITY"])
+    # print (df["HOURLYDRYBULBTEMPF"])
+    # # df["HOURLYDRYBULBTEMPF"] = pd.to_numeric(df["HOURLYDRYBULBTEMPF"].extract('(\d+)', expand=True).astype(int))
+    # # df["HOURLYDRYBULBTEMPF"] = pd.to_numeric(df["HOURLYDRYBULBTEMPF"].str.replace(r"[D]",value=r''))
+    # print("Temp")
+    # print (df["HOURLYDRYBULBTEMPF"])
+    #
+    # df["HOURLYWETBULBTEMPF"] = pd.to_numeric(df["HOURLYWETBULBTEMPF"])
+    # df["HOURLYRelativeHumidity"]  = pd.to_numeric(df["HOURLYRelativeHumidity"])
+    # df["HOURLYWindSpeed"] = pd.to_numeric(df["HOURLYWindSpeed"])
+    # df["HOURLYWindDirection"] = pd.to_numeric(df["HOURLYWindDirection"])
+    # df["HOURLYWindGustSpeed"] = pd.to_numeric(df["HOURLYWindGustSpeed"])
+
+
+    df["HOURLYVISIBILITY"] = pd.to_numeric(df["HOURLYVISIBILITY"], errors='coerce')
+    df["HOURLYDRYBULBTEMPF"] = pd.to_numeric(df["HOURLYDRYBULBTEMPF"], errors='coerce')
+    df["HOURLYRelativeHumidity"]  = pd.to_numeric(df["HOURLYRelativeHumidity"], errors='coerce')
+    df["HOURLYWindSpeed"] = pd.to_numeric(df["HOURLYWindSpeed"], errors='coerce')
+    df["HOURLYWindDirection"] = pd.to_numeric(df["HOURLYWindDirection"], errors='coerce')
+    df["HOURLYWindGustSpeed"] = pd.to_numeric(df["HOURLYWindGustSpeed"], errors='coerce')
+
+    matrix = df.fillna(0).as_matrix().mean(axis=0)
+    print(matrix)
+    row.extend(matrix)
     return row
+
+    #
+    #
+    # # pd.to_numeric(df["HOURLYVISIBILITY"], errors='coerce')
+    # # pd.to_numeric(df["HOURLYDRYBULBTEMPF"], errors='coerce')
+    # # pd.to_numeric(df["HOURLYWETBULBTEMPF"], errors='coerce')
+    # # pd.to_numeric(df["HOURLYRelativeHumidity"], errors='coerce')
+    # # pd.to_numeric(df["HOURLYWindSpeed"], errors='coerce')
+    # # pd.to_numeric(df["HOURLYWindDirection"], errors='coerce')
+    # # pd.to_numeric(df["HOURLYWindGustSpeed"], errors='coerce')
+    #
+    #
+    # # print("Temp")
+    # # print (df["HOURLYDRYBULBTEMPF"])
+    # # df = df.fillna(0.0)
+    # # print(df.dtypes)
+    # # print(df)
+    #
+    #
+    #
+    # # exit (0)
+    # # matrix = pd.to_numeric(df, errors='coerce').fillna(0).astype(np.int64)
+    # # matrix = df.fillna(0)
+    # # matrix = df.fillna(0).values()
+    # # print(matrix)
+    # # matrix = df.fillna(0).as_matrix()
+    # # matrix = pd.to_numerics(matrix)
+    # # matrix = matrix.astype(float)
+    # # matrix = float(matrix)
+    # # print(matrix)
+    #
+    #
+    #
+    # df["mean"] = np.array(df.mean(axis=0))
+    #
+    # # matrix = np.array(df.mean(axis=0)).reshape(1, df.shape[1])
+    # # matrix = list(df.mean(axis=0))
+    # # matrix = df.mean(axis=0).values()
+    # print (df["mean"])
+    # # print (matrix)
+    # exit(0)
+    # # matrix = matrix.mean(axis=0)
+    # # matrix = matrix.sum(axis=0)
+    #
+    # # print (matrix)
+    # row.extend(matrix)
+    # # row.extend( matrix.astype(float).mean(axis=0))
+    # # np.mean()
+    # # print(row)
+    # # row.extend( df.fillna(0).as_matrix().flatten('F'))
+    # return row
+
 
 
 def similar_days(weather_data, date):
-    # for key, value in weather_data.items():
     atl = weather_data["ATL"]
     tx = weather_data["TX"]
-    list_of_dates = tx['DATE'].unique()
+
+
+    # atl_data =  process_data( atl.loc[(date == atl["DATE"])], date)
+
+    # print( atl.loc[(date == atl["DATE"])], date)
+
+    X =  process_data( atl.loc[(date == atl["DATE"])], date)
+    # print(X)
 
     tx_matrix = []
+    list_of_dates = tx['DATE'].unique()
     for i in range(len(list_of_dates)):
-        # print(list_of_dates[i])
-        date = list_of_dates[i]
-        data = tx.loc[(date == tx["DATE"])]
-        tx_matrix.append( process_data(data, date))
-    print(tx_matrix)
+        data = tx.loc[(list_of_dates[i] == tx["DATE"])]
+        # tx_matrix.append( process_data(data, list_of_dates[i]))
+        Y = process_data(data, list_of_dates[i])
+        # results = cosine_similarity(X[1:], Y[1:])
+        print(Y[1:])
+        # exit(0)
+        results = cos(X[1:], Y[1:])
+        exit(0)
+        break
 
 
 
